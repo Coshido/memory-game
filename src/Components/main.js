d86e3ebc7e3d81e6257238b5f8986386165bbe38
Coshido <coshido@gmail.com>
2023-02-09 16:21:14 +0100

    add 2 new card to cardArray

--- src/Components/main.js
+++ src/Components/main.js
@@ -5,14 +5,16 @@ import "../style.css";
 const Main = (props) => {
   const [clickedCards, setClickedCards] = React.useState([""]);
   const cardArray = [
-    { name: "bmo", url: "/Media/BMO.png" },
-    { name: "flame", url: "/Media/FlamePrincess.png" },
-    { name: "jake", url: "/Media/Jakesalad.png" },
-    { name: "marceline", url: "/Media/Marceline1.png" },
-    { name: "finn", url: "/Media/Original_Finn.png" },
-    { name: "ice", url: "/Media/Original_Ice_King.png" },
-    { name: "peppermint", url: "/Media/Peppermint_Butler.png" },
-    { name: "bubble", url: "/Media/Princess_Bubblegum.png" },
+    { name: "BMO", url: "/Media/BMO.png" },
+    { name: "Flame Princess", url: "/Media/FlamePrincess.png" },
+    { name: "Jake the dog", url: "/Media/Jakesalad.png" },
+    { name: "Marceline", url: "/Media/Marceline1.png" },
+    { name: "Finn the adventurer", url: "/Media/Original_Finn.png" },
+    { name: "Ice King", url: "/Media/Original_Ice_King.png" },
+    { name: "Peppermint", url: "/Media/Peppermint_Butler.png" },
+    { name: "Princess Bubblegum", url: "/Media/Princess_Bubblegum.png" },
+    { name: "Gunter", url: "/Media/Gunter.png" },
+    { name: "Earl of Lemongrab", url: "/Media/Earl.png" },
   ];
   const sortedCardArray = cardArray.sort(() => Math.random() - 0.5);
 
@@ -32,7 +34,7 @@ const Main = (props) => {
         return (
           <Card
             image={card.url}
-            clickedCards={clickedCards}
+            cardName={card.name}
             clickedCheck={() => clickedCheck(card)}
           />
         );
