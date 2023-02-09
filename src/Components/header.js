311fc191655d84afe3baa13549c77be19c436b9f
Coshido <coshido@gmail.com>
2023-02-09 16:33:23 +0100

    fix header element positions

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 function Header(props) {
   return (
     <header>
-      <img
-        src={process.env.PUBLIC_URL + "/Media/Adventure-Time-Logo.png"}
-        className="header-image"
-      />
+      <div>
+        <img
+          src={process.env.PUBLIC_URL + "/Media/Adventure-Time-Logo.png"}
+          className="header-image"
+        />
 
-      <p className="header-title">Memory Game</p>
-
+        <p className="header-title">Memory Game</p>
+      </div>
       <div>
         <p className="best-score">Best Score: {props.bestScore}</p>
         <p className="current-score">Current Score:{props.score}</p>
